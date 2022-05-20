2a2890abefb18a7356ddd0b836cd64a50e9a218e
jins <rhrhkdwls24@naver.com>
2022-05-20 20:01:04 +0900

    Java baseball game (charAt,indexOf)

--- java0520/src/main/java/Main.java
+++ java0520/src/main/java/Main.java
@@ -1,22 +1,119 @@
-class Calculation {
-    int add(int x, int y) {
-        int result = x + y;
-        return result;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+abstract class Player{
+    private String name;
+    private String initNumber;
+    private List<String> fightList = new LinkedList<String>();
+    private Boolean isEnd;
+    public Player(String name, String initNumber) {
+        this.name = name;
+        this.initNumber = initNumber;
+        this.isEnd = false;
+    }
+    public abstract void play(String number) throws Exception;
+    public void addFightList(String number) { fightList.add(number); }
+    public Boolean getIsEnd() { return isEnd; }
+    public void setIsEnd(Boolean isEnd) { this.isEnd = isEnd; }
+    public String getInitNumber() { return initNumber; }
+    public String getName() { return name; }
+    public void printFightList() {
+        // TODO
+    }
+}
+class PlayerATeam extends Player
+{
+    public PlayerATeam(String name, String number) {
+        super(name, number);
     }
 
-    int subtract(int x, int y) {
-        int result = x - y;
-        return result;
+    @Override
+    public void play(String number) throws Exception {
+        int strike = 0;
+        int ball = 0;
+        int out = 0;
+
+        for(int i=0; i<4; i++){
+            if(getName().charAt(i)==getInitNumber().charAt(i)){
+                strike+=1;
+            }else{
+                if(getInitNumber().indexOf(getName().charAt(i))!=-1){
+                    ball+=1;
+                }else{
+                    out+=1;
+                }
+            }
+        }
+        if(strike == 4) {
+            System.out.println("Congratulation!");
+            super.setIsEnd(true);
+        } else {
+            System.out.println("Strike : "+strike+", Ball :"+ball+", Out: "+out);
+        }
+    }
+}
+
+class PlayerBTeam extends Player
+{
+    public PlayerBTeam(String name, String number) {
+        super(name, number);
+    }
+    @Override
+    public void play(String number) throws Exception {
+        int strike = 0;
+        int ball = 0;
+        int out = 0;
+
+        for(int i=0; i<4; i++){
+            if(getName().charAt(i)==getInitNumber().charAt(i)){
+                ball+=1;
+            }else{
+                if(getInitNumber().indexOf(getName().charAt(i))!=-1){
+                    strike+=1;
+                }else{
+                    out+=1;
+                }
+            }
+        }
+
+        if(ball == 4) {
+            System.out.println("Congratulation!");
+            super.setIsEnd(true);
+        } else {
+            System.out.println("Strike : "+strike+", Ball :"+ball+", Out: "+out);
+        }
     }
 }
 
 public class Main {
-    public static void main(String[] args) {
-        Calculation calculation = new Calculation();
-        int addResult = calculation.add(100, 90);
-        int subResult = calculation.subtract(90, 70);
 
-        System.out.println("더하기 값은 " + addResult);
-        System.out.println("빼기 값은 " + subResult);
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        Player playerArray[] = {new PlayerATeam("A Team","1234"), new PlayerBTeam("B Team","5678")};
+        int checkPlayer = 0;
+
+        while(true) {
+            checkPlayer = 0;
+            for(Player player : playerArray) {
+                if (player.getIsEnd() == false) {
+                    System.out.print("Please enter a 4 digit number (" + player.getName() + ") defence : ");
+                    String number = scanner.nextLine();
+                    try {
+                        player.play(number);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                } else {
+                    checkPlayer++;
+                }
+            }
+            if(checkPlayer == playerArray.length) break;
+        }
+
+        for(Player player : playerArray) {
+            player.printFightList();
+        }
     }
 }
