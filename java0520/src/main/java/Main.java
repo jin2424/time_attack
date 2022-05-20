bd70fb10c4efa003d632c91081e9a845919a059f
jins <rhrhkdwls24@naver.com>
2022-05-20 17:32:20 +0900

    calculation

--- java0520/src/main/java/Main.java
+++ java0520/src/main/java/Main.java
@@ -1,6 +1,22 @@
+class Calculation {
+    int add(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+
+    int subtract(int x, int y) {
+        int result = x - y;
+        return result;
+    }
+}
+
 public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("Hello World!");
+    public static void main(String[] args) {
+        Calculation calculation = new Calculation();
+        int addResult = calculation.add(100, 90);
+        int subResult = calculation.subtract(90, 70);
+
+        System.out.println("더하기 값은 " + addResult);
+        System.out.println("빼기 값은 " + subResult);
     }
 }
