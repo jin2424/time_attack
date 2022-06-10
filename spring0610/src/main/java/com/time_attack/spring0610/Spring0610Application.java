5b56fa691591858113ad5e7540c600a66b06d653
KGJ24 <rhrhkdwls24@naver.com>
2022-06-10 19:00:23 +0900

    spring0610

---
+++ spring0610/src/main/java/com/time_attack/spring0610/Spring0610Application.java
@@ -0,0 +1,13 @@
+package com.time_attack.spring0610;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring0610Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring0610Application.class, args);
+	}
+
+}
---
+++ spring0610/src/test/java/com/time_attack/spring0610/Spring0610ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.time_attack.spring0610;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring0610ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
